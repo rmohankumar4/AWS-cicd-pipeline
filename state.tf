919322aac0a00a4fec0bf2a8bb94620f2c00b38b
FNU Mohan Kumar Rashmi <rmohankumar4@dxc.com>
2022-05-23 12:39:10 -0400

    initial

--- state.tf
+++ state.tf
@@ -1,8 +1,18 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
   backend "s3" {
       bucket = "dti-aws-pipeline"
       encrypt = true
       key =  "terraform.tfstate"
       region =  "us-east-1"
+  }
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAXXZ7KUAM3TCOC37Z"
+  secret_key = "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"
   }
 }
